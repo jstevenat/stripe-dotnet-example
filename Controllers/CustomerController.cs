db00bb0dc77a8f1227427ce6d6eb06c87589ce84
Steven Avendano <stevenavendanio@gmail.com>
2024-09-18 18:53:47 +1000

    Add EF Core Add initial DB context Add Customer DB - Create endpoint

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using stripe_dotnet_example.Entities;
+
+namespace stripe_dotnet_example.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomerController : ControllerBase
+	{
+		private readonly DbContext _dbContext;
+
+		public CustomerController(DbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create([FromBody] Customer request)
+		{
+			_dbContext.Add<Customer>(request);
+			await _dbContext.SaveChangesAsync();
+
+			return Ok();
+		}
+	}
+}
---
+++ DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using stripe_dotnet_example.Entities;
+
+public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+	public DbContext(DbContextOptions<DbContext> options)
+		  : base(options)
+	{
+	}
+
+	public DbSet<Customer> Customers { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+	}
+}
